45693056a188e75db64258193cd5e7cd5f9a4b20
rengzhengcodes <rzhengcontacts@gmail.com>
2020-11-23 12:30:37 -0500

    Moved improper solutions requiring constructor change to new directory.

---
+++ SetValueSolution/RationalNumber.java
@@ -0,0 +1,139 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  /**Initialize the RationalNumber with the provided values
+  *  if the denominator is 0, make the fraction 0/1 instead
+  *  If the denominator is negative, negate both numerator and denominator
+  *@param nume the numerator
+  *@param deno the denominator
+  */
+  public RationalNumber(int nume, int deno) {
+    super(nume / (double)deno);
+    if (deno == 0 || nume == 0) {
+      this.numerator = 0;
+      this.denominator = 1;
+    } else if (deno < 0) {
+      this.numerator = -nume;
+      this.denominator = -deno;
+    } else {
+      this.numerator = nume;
+      this.denominator = deno;
+    }
+    this.reduce();
+  }
+
+  public double getValue() {
+    return super.getValue();
+  }
+
+  /**
+  *@return the numerator
+  */
+  public int getNumerator() {
+    return numerator;
+  }
+  /**
+  *@return the denominator
+  */
+  public int getDenominator() {
+    return denominator;
+  }
+  /**
+  *@return a new RationalNumber that has the same numerator
+  *and denominator as this RationalNumber but reversed.
+  */
+  public RationalNumber reciprocal() {
+    return new RationalNumber(this.denominator, this.numerator);
+  }
+  /**
+  *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
+  */
+  public boolean equals(RationalNumber other) {
+    return (this.numerator == other.numerator && this.denominator == other.denominator);
+  }
+
+
+  /**
+  *@return the value expressed as "3/4" or "8/3"
+  */
+  public String toString() {
+    return this.numerator + "/" + this.denominator;
+  }
+
+  /**Calculate the GCD of two integers.
+  *@param a the first integers
+  *@param b the second integer
+  *@return the value of the GCD
+  */
+  private static int gcd(int a, int b) {
+    /*use euclids method or a better one*/
+    http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    if (a == 0 || b == 0) {
+      return 1;
+    }
+
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    if (a < b) {//Makes a the largest number
+      int c = b;
+      b = a;
+      a = c;
+    }
+
+    int remainder = a % b;
+    while (remainder != 0) {
+      a = b;
+      b = remainder;
+      remainder = a % b;
+    }
+
+    return b;
+  }
+
+  /**
+  *Divide the numerator and denominator by the GCD
+  *This must be used to maintain that all RationalNumbers are
+  *reduced after construction.
+  */
+  private void reduce() {
+    int gcd = gcd(this.numerator, this.denominator);
+    this.numerator = this.numerator / gcd;
+    this.denominator = this.denominator / gcd;
+  }
+  /******************Operations Return a new RationalNumber!!!!****************/
+  /**
+  *Return a new RationalNumber that is the product of this and the other
+  */
+  public RationalNumber multiply(RationalNumber other) {
+    return new RationalNumber(this.numerator * other.numerator, this.denominator * other.denominator);
+  }
+
+  /**
+  *Return a new RationalNumber that is the this divided by the other
+  */
+  public RationalNumber divide(RationalNumber other) {
+    return this.multiply(other.reciprocal());
+  }
+
+  /**
+  *Return a new RationalNumber that is the sum of this and the other
+  */
+  public RationalNumber add(RationalNumber other) {
+    int gcd = gcd(this.denominator, other.denominator);
+    int otherMultiply = this.denominator / gcd;
+    int thisMultiply = other.denominator / gcd;
+    return new RationalNumber(this.numerator * thisMultiply + other.numerator * otherMultiply,
+                              this.denominator * thisMultiply);
+  }
+  /**
+  *Return a new RationalNumber that this minus the other
+  */
+  public RationalNumber subtract(RationalNumber other) {
+    int gcd = gcd(this.denominator, other.denominator);
+    int otherMultiply = this.denominator / gcd;
+    int thisMultiply = other.denominator / gcd;
+    return new RationalNumber(this.numerator * thisMultiply - other.numerator * otherMultiply,
+                              this.denominator * thisMultiply);
+  }
+}
---
+++ SetValueSolution/RealNumber.java
@@ -0,0 +1,71 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v) {
+    if (Double.isInfinite(v) || Double.isNaN(v)) {
+      value = 0;
+    } else {
+      value = v;
+    }
+  }
+
+  public double getValue() {
+    return value;
+  }
+
+  public String toString() {
+    return "" + getValue();
+  }
+  //---------ONLY EDIT BELOW THIS LINE------------
+
+  /*
+  *Return true when the values are within 0.001% of eachother.
+  *Special case: if one is exactly zero, the other must be exactly zero.
+  */
+  public boolean equals(RealNumber other) {
+    double errorBars = 1E-5;
+    if (this.value == 0 || other.value == 0) {
+      if (this.value == 0 && other.value == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return Math.abs((this.value - other.value)/this.value) < errorBars;
+    }
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the sum of this and the other
+  */
+  public RealNumber add(RealNumber other){
+   //other can be ANY RealNumber, including a RationalNumber
+   //or other subclasses of RealNumber (that aren't written yet)
+   return new RealNumber(this.value + other.value);
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the product of this and the other
+  */
+  public RealNumber multiply(RealNumber other){
+    return new RealNumber(this.value * other.value);
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this divided by the other
+  */
+  public RealNumber divide(RealNumber other){
+    return new RealNumber(this.value / other.value);
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this minus the other
+  */
+  public RealNumber subtract(RealNumber other){
+    return new RealNumber(this.value - other.value);
+  }
+}
